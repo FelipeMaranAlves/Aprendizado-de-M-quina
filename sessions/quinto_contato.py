5199fda2b889814c86105eb0e8d7ac70c17a1666
fma3 <you@example.com>
2026-06-08 15:00:38 -0300

    Feat selection com rf

--- sessions/quinto_contato.py
+++ sessions/quinto_contato.py
@@ -5,12 +5,15 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
+from utils import documentar
 def rodar():
     df = pd.read_csv('data/PDF_All_feature_Clean.csv')
     df = df.drop(columns='file_path')
-    X_train, X_test, Y_train, Y_test = train_test_split(df,0.8,0.2)
-    rf = RandomForestClassifier(n_estimators=100,random_state=42)
-    rf.fit(X_train,Y_train)
+    X = df.drop(columns=["label"])
+    y = df["label"]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=2)
+    rf = RandomForestClassifier(n_estimators=100,random_state=6467)
+    rf.fit(X_train,y_train)
     importances = pd.Series(rf.feature_importances_,index=X_train.columns)
     importances.sort_values(ascending=False,inplace=True)
-    print(importances)
+    documentar("Quinto_contato_Sp3",str(importances))
