9214bbc9615a3ac015e8e81d6d75ace97e3f6c23
fma3 <felipemaranalvees40@gmail.com>
2026-06-09 17:09:52 -0300

    começo dos experimentos do isf

--- sessions/baseline.py
+++ sessions/baseline.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
 from utils import documentar
-from utilsDoProfessor import get_overall_metrics
+from utilsDoProfessor import get_overall_metrics, plot_confusion_matrix
 
 #vamos fazer o pipe basico pro baseline depois podemos tartar melhor os dados apra ter
 #um pipe mais completo como a disciplina propõe
@@ -19,29 +19,32 @@ def rodar():
     
     #divisao diferente de treino para benigno e teste incluindo ambos.
     #No momento treino e testa ambos estao com 10% do total dos dados (não balanceado)
-    X_train, _ = train_test_split(df_Bening,test_size=0.2,random_state=2)
-    _, Xy_tmp = train_test_split(df,test_size=0.2,random_state=2)
-    Xy_val, Xy_test = train_test_split(Xy_tmp,test_size=0.5)
+    RAND_STATE = 2
+    X_train, _ = train_test_split(df_Bening,test_size=0.2,random_state=RAND_STATE)
+    _, Xy_tmp = train_test_split(df,test_size=0.2,random_state=RAND_STATE)
+    Xy_val, Xy_test = train_test_split(Xy_tmp,test_size=0.5, random_state=RAND_STATE)
+
+    y_train = X_train['label']
+    X_train = X_train.drop(columns = ['label'])
+    y_val = Xy_val['label']
+    X_val = Xy_val.drop(columns = ['label'])
+    y_test = Xy_test['label']
+    X_test =  Xy_test.drop(columns = ['label'])
+
 
     #normalizando bazeado em z
     #normalizando com a mesma escala do treino para nao ter dataleak!
     std_scaler = StandardScaler()
     std_scaler = std_scaler.fit(X_train)
     nomr_X_train = std_scaler.transform(X_train)
-    norm_Xy_val = std_scaler.transform(Xy_val)
-    norm_Xy_test = std_scaler.transform(Xy_test)
+    norm_X_val = std_scaler.transform(X_val)
+    norm_X_test = std_scaler.transform(X_test)
 
-    isf_model = IsolationForest(n_estimators=100,random_state=67).fit(nomr_X_train)
-    
-    predicoes = isf_model.predict(df)
-    predicoes[predicoes == 1] = 1
-    predicoes[predicoes == -1] = 0
-    # string_T = "True\n"
-    # string_T += str(df['label'].head(20).to_list())
-    # documentar("baseline_1_saida",string_T)
-    # string_P = "Predito\n"
-    # string_P += str(predicoes[0:20].tolist())
-    # documentar("baseline_1_saida",string_P)
-    get_overall_metrics()
+    for i in range(4):
+        CONTAMINATION = (0.05 - (0.01 * (i+1)))
+        isf_model = IsolationForest(n_estimators=100,random_state=RAND_STATE,contamination=CONTAMINATION).fit(nomr_X_train)
 
-    
+        predicoes_val = isf_model.predict(norm_X_val)
+        predicoes_val[predicoes_val == 1] = 1
+        predicoes_val[predicoes_val == -1] = 0
+        documentar("IsolationForest1",f"Metricas com o valor de contaminacao: {CONTAMINATION}\n"+str(get_overall_metrics(y_val,predicoes_val)))
